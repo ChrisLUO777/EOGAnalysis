387b218262c26993b7351c54ee214e8315925153
LUOShiqing <2386004278@qq.com>
2019-05-16 16:22:18 -0400

    optimize linear regression module

--- 10sversion/information fusion/fix/linearreg/EOGfixfusionlinear.m
+++ 10sversion/information fusion/fix/linearreg/EOGfixfusionlinear.m
@@ -1,9 +1,9 @@
 close all; clear;
 load("EOGfixtrainset.mat");
 load("EOGfixtestset.mat");
-X=EOGfixtrainset(:,1:28);
+X=EOGfixtrainset(:,1:46);
 y=EOGfixtrainset(:,47);
-Xtest=EOGfixtestset(:,1:28);
+Xtest=EOGfixtestset(:,1:46);
 ytest=EOGfixtestset(:,47);
 mtest=size(Xtest,1);
 
@@ -28,74 +28,40 @@ for i=1:size(ytest,1)
                 FN=FN+1;
     end
 end
+precise=TP/(TP+FP);
+recall=TP/(TP+FN);
+Fscore=2*precise*recall/(precise+recall);
 fprintf('TP=%f, FN=%f, FP=%f, TN=%f \n',TP,FN,FP,TN);
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Fscore=%f \n',Fscore);
 %%
 %EER
-RN=sum(ytest);
-RP=size(ytest,1)-RN;
-
-threshold=-1:0.00001:2;
-TPR=zeros(1,size(threshold,2));
-FPR=zeros(1,size(threshold,2));
-count=1;
-for k=threshold
-    TP=0;
-    FP=0;
-    for i=1:size(ytest,1)
-        if((score(i,1)<k) && (ytest(i,1)==0))%TP
-                TP=TP+1;
-        elseif((score(i,1)<k) && (ytest(i,1)==1))%FP
-                FP=FP+1;
-        end
-    end
-    TPR(1,count)=(TP/RP);
-    FPR(1,count)=(FP/RN);
-    count=count+1;
-end
-
-plot(FPR,TPR);
-hold on;
-plot(FPR,TPR,'.');
-plot(0:0.01:1,1:-0.01:0,'r');
-grid on;
-xlabel('False Positive Rate');
-ylabel('True Positive Rate');
-axis([0 1 0 1]);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-%%
-%F score
-close all;
-
-threshold=-1:0.00001:2;
-Fscore=zeros(1,size(threshold,2));
-count=1;
-for k=threshold
-    TP=0;
-    FP=0;
-    FN=0;
-    for i=1:size(ytest,1)
-        if((score(i,1)<k) && (ytest(i,1)==0))%TP
-                TP=TP+1;
-        elseif((score(i,1)<k) && (ytest(i,1)==1))%FP
-                FP=FP+1;
-        elseif((score(i,1)>k) && (ytest(i,1)==0))%FN
-                FN=FN+1;        
-        end
-    end
-    precise=TP/(TP+FP);
-    recall=TP/(TP+FN);
-    Fscore(1,count)=2*precise*recall/(precise+recall);
-    count=count+1;
-end
-
-plot(threshold,Fscore);
-hold on;
-plot(threshold,Fscore,'.');
-grid on;
-xlabel('threshold');
-ylabel('fscore');
-axis([-1 2 0 1]);
+% RN=sum(ytest);
+% RP=size(ytest,1)-RN;
+% 
+% threshold=-1:0.0001:2;
+% TPR=zeros(1,size(threshold,2));
+% FPR=zeros(1,size(threshold,2));
+% count=1;
+% for k=threshold
+%     TP=0;
+%     FP=0;
+%     for i=1:size(ytest,1)
+%         if((score(i,1)<k) && (ytest(i,1)==0))%TP
+%                 TP=TP+1;
+%         elseif((score(i,1)<k) && (ytest(i,1)==1))%FP
+%                 FP=FP+1;
+%         end
+%     end
+%     TPR(1,count)=(TP/RP);
+%     FPR(1,count)=(FP/RN);
+%     count=count+1;
+% end
+% 
+% plot(FPR,TPR);
+% hold on;
+% plot(FPR,TPR,'.');
+% plot(0:0.01:1,1:-0.01:0,'r');
+% grid on;
+% xlabel('False Positive Rate');
+% ylabel('True Positive Rate');
+% axis([0 1 0 1]);
--- 10sversion/information fusion/fix/svm/EOGfixsvmfusion.m
+++ 10sversion/information fusion/fix/svm/EOGfixsvmfusion.m
@@ -2,54 +2,12 @@ close all; clear;
 load("EOGfixtrainset.mat");
 load("EOGfixtestset.mat");
 
-d=EOGfixtrainset(:,1:46);
-f=EOGfixtrainset(:,47);
-K=46;
-fea=mrmr_mid_d(d,f,K);
-Fscore=0;
-bestfeature=[1:28];
-mRMRfscore=0;
-%%
-%mRMR
-% for count=2:46
-%     X=EOGfixtrainset(:,fea(1:count));
-%     y=EOGfixtrainset(:,47);
-% 
-%     Xtest=EOGfixtestset(:,fea(1:count));
-%     ytest=EOGfixtestset(:,47);
-% 
-%     
-%     %svm
-%     model=fitcsvm(X,y,'KernelFunction','gaussian');
-%     [~,score] = predict(model,Xtest);
-% 
-%     %mRMR(Fscore)
-%     TP=0;
-%     FP=0;
-%     FN=0;
-%     for i=1:size(ytest,1)
-%         if((score(i,2)<k) && (ytest(i,1)==0))%TP
-%                 TP=TP+1;
-%         elseif((score(i,2)<k) && (ytest(i,1)==1))%FP
-%                 FP=FP+1;
-%         elseif((score(i,2)>k) && (ytest(i,1)==0))%FN
-%                 FN=FN+1;        
-%         end
-%     end
-%     precise=TP/(TP+FP);
-%     recall=TP/(TP+FN);
-%     tempFscore=2*precise*recall/(precise+recall);
-%     if tempFscore>mRMRfscore
-%        mRMRfscore=tempFscore;
-%        bestfeature=fea(1:count);
-%     end
-% end
 %%
 %train SVM
-X=EOGfixtrainset(:,bestfeature);
+X=EOGfixtrainset(:,1:28);
 y=EOGfixtrainset(:,47);
 m = size(X, 1);
-Xtest=EOGfixtestset(:,bestfeature);
+Xtest=EOGfixtestset(:,1:28);
 ytest=EOGfixtestset(:,47);
 mtest=size(Xtest,1);
 
@@ -61,100 +19,55 @@ TP=0;
 FP=0;
 TN=0;
 FN=0;
-% for i=1:size(ytest,1)
-%     if((label(i,1)==0) && (ytest(i,1)==0))%TP
-%                 TP=TP+1;
-%     elseif((label(i,1)==0) && (ytest(i,1)==1))%FP
-%                 FP=FP+1;
-%     elseif((label(i,1)==1) && (ytest(i,1)==1))%TN
-%                 TN=TN+1;
-%     elseif((label(i,1)==1) && (ytest(i,1)==0))%FN
-%                 FN=FN+1;
-%     end
-% end
 for i=1:size(ytest,1)
-    if((score(i,2)<0.7275) && (ytest(i,1)==0))%TP
+    if((label(i,1)==0) && (ytest(i,1)==0))%TP
                 TP=TP+1;
-    elseif((score(i,2)<0.7275) && (ytest(i,1)==1))%FP
+    elseif((label(i,1)==0) && (ytest(i,1)==1))%FP
                 FP=FP+1;
-    elseif((score(i,2)>0.7275) && (ytest(i,1)==1))%TN
+    elseif((label(i,1)==1) && (ytest(i,1)==1))%TN
                 TN=TN+1;
-    elseif((score(i,2)>0.7275) && (ytest(i,1)==0))%FN
+    elseif((label(i,1)==1) && (ytest(i,1)==0))%FN
                 FN=FN+1;
     end
 end
+precise=TP/(TP+FP);
+recall=TP/(TP+FN);
+Fscore=2*precise*recall/(precise+recall);
 fprintf('TP=%f, FN=%f, FP=%f, TN=%f \n',TP,FN,FP,TN);
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Fscore=%f \n',Fscore);
 %%
 %EER
-RN=sum(ytest);
-RP=size(ytest,1)-RN;
-
-threshold=-5:0.00001:5;
-TPR=zeros(1,size(threshold,2));
-FPR=zeros(1,size(threshold,2));
-count=1;
-bestth=0;
-for k=threshold
-    TP=0;
-    FP=0;
-    for i=1:size(ytest,1)
-        if((score(i,2)<k) && (ytest(i,1)==0))%TP
-                TP=TP+1;
-        elseif((score(i,2)<k) && (ytest(i,1)==1))%FP
-                FP=FP+1;
-        end
-    end
-    TPR(1,count)=(TP/RP);
-    FPR(1,count)=(FP/RN);
-    if TPR(1,count)<=(1-FPR(1,count))
-       bestth=k;
-    end
-    count=count+1;
-end
-
-plot(FPR,TPR);
-hold on;
-plot(FPR,TPR,'.');
-plot(0:0.01:1,1:-0.01:0,'r');
-grid on;
-xlabel('False Positive Rate');
-ylabel('True Positive Rate');
-axis([0 1 0 1]);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-%%
-%F score
-close all;
-
-threshold=-5:0.00001:5;
-Fscore=zeros(1,size(threshold,2));
-count=1;
-for k=threshold
-    TP=0;
-    FP=0;
-    FN=0;
-    for i=1:size(ytest,1)
-        if((score(i,2)<k) && (ytest(i,1)==0))%TP
-                TP=TP+1;
-        elseif((score(i,2)<k) && (ytest(i,1)==1))%FP
-                FP=FP+1;
-        elseif((score(i,2)>k) && (ytest(i,1)==0))%FN
-                FN=FN+1;        
-        end
-    end
-    precise=TP/(TP+FP);
-    recall=TP/(TP+FN);
-    Fscore(1,count)=2*precise*recall/(precise+recall);
-    count=count+1;
-end
-
-plot(threshold,Fscore);
-hold on;
-plot(threshold,Fscore,'.');
-grid on;
-xlabel('threshold');
-ylabel('fscore');
-axis([0 1 0 1]);
+% RN=sum(ytest);
+% RP=size(ytest,1)-RN;
+% 
+% threshold=-5:0.00001:5;
+% TPR=zeros(1,size(threshold,2));
+% FPR=zeros(1,size(threshold,2));
+% count=1;
+% bestth=0;
+% for k=threshold
+%     TP=0;
+%     FP=0;
+%     for i=1:size(ytest,1)
+%         if((score(i,2)<k) && (ytest(i,1)==0))%TP
+%                 TP=TP+1;
+%         elseif((score(i,2)<k) && (ytest(i,1)==1))%FP
+%                 FP=FP+1;
+%         end
+%     end
+%     TPR(1,count)=(TP/RP);
+%     FPR(1,count)=(FP/RN);
+%     if TPR(1,count)<=(1-FPR(1,count))
+%        bestth=k;
+%     end
+%     count=count+1;
+% end
+% 
+% plot(FPR,TPR);
+% hold on;
+% plot(FPR,TPR,'.');
+% plot(0:0.01:1,1:-0.01:0,'r');
+% grid on;
+% xlabel('False Positive Rate');
+% ylabel('True Positive Rate');
+% axis([0 1 0 1]);
