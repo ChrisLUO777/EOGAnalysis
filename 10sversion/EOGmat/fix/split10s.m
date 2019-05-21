dba059043049e18e12a9be046fb2c66a54cdb8f5
LUOShiqing <2386004278@qq.com>
2019-05-20 21:47:04 -0400

    processed new dataset.

--- 10sversion/EOGmat/fix/split10s.m
+++ 10sversion/EOGmat/fix/split10s.m
@@ -1,12 +1,13 @@
 %new way to split data and process data into 10s, more convenient.
 close all; clear;
-subname=["61" "62" "63" "64" "65" "66" "67" "68" "69" "70" "71" "72" "73" "74" "75" "76" "77" "78"];
+subname=["41" "42" "43" "44" "45" "46" "47" "48" "49" "50" "51" "52" "53" "54" "55" "56" "57" "58" "59" "60" ...
+    "61" "62" "63" "64"];
 for name=subname
     load(".\"+name+"\EOG"+name+"fix.mat");
     length=size(data,1);
     for i=1:10
-        firstco=data((length*i/11-999):(length*i/11+1000),1);
-        secondco=data((length*i/11-999):(length*i/11+1000),2);
+        firstco=data((1+(length-2000)*(i-1)/9):(2000+(length-2000)*(i-1)/9),1);
+        secondco=data((1+(length-2000)*(i-1)/9):(2000+(length-2000)*(i-1)/9),2);
         data10s=[firstco secondco];
         mkdir(".\"+name+"\EOG"+name+"fix"+i);
         addpath(genpath(pwd));
